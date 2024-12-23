f9e24c3f902691174cf7f633be6f10f0e1e11767
sudipta <sudipta_seal08@yahoo.com>
2024-12-23 23:08:26 +0530

    Form submit

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    name= models.CharField(max_length=122)
+    email=models.CharField(max_length=122)
+    phone=models.CharField(max_length=12)
+    desc=models.TextField()
+    date=models.DateField()
+
+#Show entry in admin panel by name
+    def __str__(self):
+        return self.name
--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render,HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def index(request) :
@@ -18,6 +21,16 @@ def services(request):
     #return HttpResponse('this is services page')
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+        desc = request.POST.get("desc")
+        contact = Contact(name = name, email = email, phone = phone, desc = desc,
+                          date = datetime.today())
+        contact.save()
+        messages.success(request, "Your message has been sent.")
+
     return render(request, "contact.html")
     #return  HttpResponse('this is contact page')
 
